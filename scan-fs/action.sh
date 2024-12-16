d16d6a03a82818218118dfa66a7e86c7c231ef2c
unchaptered <workstation19961002@gmail.com>
2024-12-16 22:14:18 +0900

    fix : scan-fs/action.sh

--- scan-fs/action.sh
+++ scan-fs/action.sh
@@ -8,5 +8,8 @@ time -p (
         HCP_PROJECT_ID=$HCP_PROJECT_ID \
         HCP_CLIENT_ID=$HCP_CLIENT_ID \
         HCP_CLIENT_SECRET=$HCP_CLIENT_SECRET \
-        vault-radar scan folder -p / -o action.log --help
+        vault-radar scan folder \
+            -p ./ \
+            -f sarif        \
+            -o sarif.log
 )
